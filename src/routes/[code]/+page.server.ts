d2805c3e06023cf5332c76489a137d435e6234e6
Rudin Alexandr <alexandr.rudin.my@gmail.com>
2023-07-16 22:46:54 +0200

    debug blog

--- src/routes/[code]/+page.server.ts
+++ src/routes/[code]/+page.server.ts
@@ -1,10 +1,23 @@
 const VITE_API_URL = import.meta.env.VITE_API_URL;
+import * as Sentry from '@sentry/sveltekit';
 
 export const load = async ({ params, fetch }) => {
-  const { code } = params;
-  const response = await fetch(`${VITE_API_URL}/articles?language=${code}`);
-  const articles = await response.json();
-  return {
-    articles: articles.list
+  const transaction = Sentry.startTransaction({
+    name: 'Debug blog issue'
+  });
+
+  Sentry.configureScope((scope) => {
+    scope.setSpan(transaction);
+  });
+
+  try {
+    const { code } = params;
+    const response = await fetch(`${VITE_API_URL}/articles?language=${code}`);
+    const articles = await response.json();
+    return {
+      articles: articles.list
+    }
+  } finally {
+    transaction.finish();
   }
 }
